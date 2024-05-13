8cee3c1feabac7304332020d9ea47d6c5d6314f7
LennardMarx <l.marx@gmx.de>
2024-05-14 00:29:27 +0200

    raycaster engine project added (textures not working)

--- js/hide_iframe.js
+++ js/hide_iframe.js
@@ -1,6 +1,7 @@
 function hideIframe(textContainerId, iframeId, url) {
   const iframe = document.getElementById(iframeId);
   const textContainer = document.getElementById(textContainerId);
+  let timer;
 
   function loadIframe() {
     console.log("clicked!");
@@ -10,13 +11,21 @@ function hideIframe(textContainerId, iframeId, url) {
   }
 
   function unloadIframe() {
-    iframe.src = '';
-    textContainer.style.display = 'block';
-    iframe.addEventListener('click', loadIframe);
+    timer = setTimeout(function() {
+      iframe.src = '';
+      textContainer.style.display = 'block';
+      iframe.addEventListener('click', loadIframe);
+    }, 2000);
+  }
+
+  function resetTimer() {
+    clearTimeout(timer); // Clear previous timer
+    // unloadIframe(); // Start a new timer
   }
 
   // iframe.addEventListener('mouseenter', loadIframe);
   // iframe.addEventListener('mouseenter', loadIframe);
   textContainer.addEventListener('click', loadIframe);
   iframe.addEventListener('mouseleave', unloadIframe);
+  iframe.addEventListener('mouseenter', resetTimer); // Reset timer when mouse reenters iframe
 }
