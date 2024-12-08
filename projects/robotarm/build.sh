592ebda21f9d50c76ad6e1df3da6367c9231637a
LennardMarx <l.marx@gmx.de>
2024-12-08 16:28:10 +0100

    added robotarm as submodule

--- projects/robotarm/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-emcc src/main.cpp -s WASM=1 -s USE_SDL=2 -O3 -o robotarm.js
-# emcc src/main.cpp -s WASM=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file resources -o robotarm.js
--- projects/robotarm_standalone/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/sim_loop.h"
-
-int main() {
-  SimLoop simLoop;
-  simLoop.run();
-
-  return 0;
-}
